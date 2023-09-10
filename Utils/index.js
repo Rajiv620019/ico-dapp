ab92202ca445499470ad1ed2d29e722c4074724e
Rajiv S <rajiv620019@gmail.com>
2023-09-10 13:40:43 +0530

    Connect Wallet

--- Utils/index.js
+++ Utils/index.js
@@ -23,3 +23,20 @@ export const isWalletConnected = async () => {
     console.log(error);
   }
 };
+
+// Connect Wallet
+export const connectWallet = async () => {
+  try {
+    if (!window.ethereum) return console.log("Wallet not found");
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    const firstAccount = accounts[0];
+    window.location.reload();
+    return firstAccount;
+  } catch (error) {
+    console.log(error);
+  }
+};
