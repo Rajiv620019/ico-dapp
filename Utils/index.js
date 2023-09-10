f6d01c04fa66ecae0ad35e3ead1e9113699891cf
Rajiv S <rajiv620019@gmail.com>
2023-09-10 12:08:10 +0530

    imported the contract  infos

---
+++ Utils/index.js
@@ -0,0 +1,9 @@
+import { ethers } from "ethers";
+import Web3Modal from "web3modal";
+
+import {
+  TOKEN_CONTRACT_ADDRESS,
+  TOKEN_CONTRACT_ABI,
+  TOKEN_SALE_CONTRACT_ADDRESS,
+  TOKEN_SALE_CONTRACT_ABI,
+} from "../Context/constants";
