ad97aecd69fdbd5cc59be178d17786aecfbb6ea3
Rajiv S <rajiv620019@gmail.com>
2023-09-11 14:24:18 +0530

    Hooks setup

--- Context/index.js
+++ Context/index.js
@@ -12,6 +12,13 @@ import {
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState("");
+  const [nativeTokenBalance, setNativeTokenBalance] = useState("");
+  const [tokenHolders, setTokenHolders] = useState([]);
+  const [tokenSale, setTokenSale] = useState("");
+  const [currentHolder, setCurrentHolder] = useState("");
+
   const Token_Ico = "Token Sale Dapp";
   return (
     <StateContext.Provider value={{ Token_Ico }}>
