3993f32c9ba7167f5f097e66c04a919f5e8333f9
Rajiv S <rajiv620019@gmail.com>
2023-09-11 14:30:36 +0530

    fetch account and balance

--- Context/index.js
+++ Context/index.js
@@ -19,6 +19,21 @@ export const StateContextProvider = ({ children }) => {
   const [tokenSale, setTokenSale] = useState("");
   const [currentHolder, setCurrentHolder] = useState("");
 
+  // Fetch initial data
+  const fetchInitialData = async () => {
+    try {
+      // Check if wallet is connected and get address
+      const account = await isWalletConnected();
+
+      // Get balance of connected wallet
+      const balance = await getBalance();
+      setBalance(ethers.utils.formatEther(balance.toString()));
+      setAddress(account);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const Token_Ico = "Token Sale Dapp";
   return (
     <StateContext.Provider value={{ Token_Ico }}>
