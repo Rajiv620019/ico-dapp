51cab4e830f0ac6e0b2057310c3ab82f5ee6fd71
Rajiv S <rajiv620019@gmail.com>
2023-09-14 00:15:39 +0530

    Imports

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,59 @@
 import React from "react";
 import { useStateContext, transferNativeToken } from "../Context/index";
+import {
+  About,
+  ArrowUp,
+  Banner,
+  Blog,
+  Client,
+  Contact,
+  Distribution,
+  Faq,
+  Footer,
+  Header,
+  Loader,
+  MobileApp,
+  Service,
+  Team,
+  TimeLine,
+  TokenSale,
+} from "../Components/index";
 
 const index = () => {
-  const { Token_Ico, transferNativeToken } = useStateContext();
+  const {
+    transferNativeToken,
+    buyToken,
+    connectWallet,
+    setAddress,
+    Token_Ico,
+    currentHolder,
+    tokenSale,
+    tokenHolders,
+    nativeToken,
+    balance,
+    address,
+  } = useStateContext();
   return (
-    <div>
-      <h1> {Token_Ico} </h1>{" "}
-      <button onClick={transferNativeToken}> Buy Token </button>{" "}
+    <div className="v_dark">
+      <Header
+        address={address}
+        setAddress={setAddress}
+        connectWallet={connectWallet}
+      />
+      <Banner transferNativeToken={transferNativeToken} />
+      <Service />
+      <About />
+      <TokenSale buyToken={buyToken} tokenSale={tokenSale} />
+      {/* <TimeLine /> */}
+      <Distribution />
+      <MobileApp />
+      <Team />
+      {/* <Client />
+      <Blog /> */}
+      <Faq />
+      <Contact />
+      <Footer />
+      {/* <ArrowUp /> */}
     </div>
   );
 };
